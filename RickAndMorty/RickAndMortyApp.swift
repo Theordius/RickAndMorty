ac2f3231fd0d03caa26f2e31b1fedef7a55bd443
Theordius <87307483+Theordius@users.noreply.github.com>
2023-07-24 11:18:32 +0200

    Initial Commit

---
+++ RickAndMorty/RickAndMortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyApp.swift
+//  RickAndMorty
+//
+//  Created by Rafał Gęsior on 24/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
