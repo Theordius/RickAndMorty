2c2aa2e39d803a9c9f200df7f37f383901eb6997
Theordius <87307483+Theordius@users.noreply.github.com>
2023-07-27 12:55:09 +0200

    bugFix: make tabBar shown alwaays the same

--- RickAndMorty/View/Home/AppView.swift
+++ RickAndMorty/View/Home/AppView.swift
@@ -9,45 +9,45 @@ import SwiftUI
 
 struct AppView: View {
     //MARK: - PROPERTIES
-    let tabBarAppearance = UITabBarAppearance()
-    
+ 
     //MARK: - BODY
     var body: some View {
         TabView {
-            ContentView()
-                .tabItem{
-                    Image(systemName:"house.fill")
-                    Text(String(localized: "About"))
-                }
-            
-            EpisodesView()
-                .tabItem {
-                    Image(systemName:"list.star")
-                    Text(String(localized: "Episodes"))
-                }
-            
-            AliveCharactersView()
-                .tabItem {
-                    Image(systemName:"heart.fill")
-                    Text(String(localized: "Alive"))
-                }
-            
-            DeadCharactersView()
-                .tabItem {
-                    Image(systemName:"heart.slash.fill")
-                    Text(String(localized: "Dead"))
-                        .foregroundColor(Color.white)
-                }
+            Group {
+                ContentView()
+                    .tabItem{
+                        Image(systemName:"house.fill")
+                        Text(String(localized: "About"))
+                    }
+                
+                EpisodesView()
+                    .tabItem {
+                        Image(systemName:"list.star")
+                        Text(String(localized: "Episodes"))
+                    }
+                
+                AliveCharactersView()
+                    .tabItem {
+                        Image(systemName:"heart.fill")
+                        Text(String(localized: "Alive"))
+                    }
+                
+                DeadCharactersView()
+                    .tabItem {
+                        Image(systemName:"heart.slash.fill")
+                        Text(String(localized: "Dead"))
+                    }
+            }
+//                        .toolbarBackground(Color.white, for: .tabBar)
+//                        .toolbar(.visible, for: .tabBar)
+        
         }
-        .accentColor(Color.primary)
+        .accentColor(Color.black)
         .onAppear {
-            //tabBarAppearance.configureWithOpaqueBackground()
-            tabBarAppearance.backgroundColor = UIColor.white
-            tabBarAppearance.stackedLayoutAppearance.normal.iconColor = UIColor.black
-            tabBarAppearance.stackedLayoutAppearance.selected.iconColor = UIColor.black
-            
-            UITabBar.appearance().standardAppearance = tabBarAppearance
-        }
+                UITabBar.appearance().backgroundColor = UIColor.systemBackground
+                UITabBar.appearance().isTranslucent = true
+           
+            }
     }
 }
 
